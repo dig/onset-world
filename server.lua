04de3dd45d89758dcc49ae1dc066712580290de4
Joseph <joseph42@live.co.uk>
2019-12-17 01:19:32 +0000

    Add support for doors

--- server.lua
+++ server.lua
@@ -1,5 +1,26 @@
 WorldLoaded = false
 
+local DoorConfig = {
+  [6]=180,
+  [17]=180,
+  [18]=180,
+  [19]=180,
+  [26]=180,
+  [27]=180,
+  [28]=180,
+  [29]=180,
+  [30]=180,
+  [31]=180,
+  [32]=180,
+  [33]=180,
+  [34]=180,
+  [35]=0,
+  [36]=270,
+  [37]=270,
+  [38]=270,
+  [39]=270
+}
+
 function World_LoadWorld()
   if WorldLoaded then return end
   if IsPackageStarted('sandbox') then return error('ERROR: Not loading world since sandbox package is enabled, this package should only be used when sandbox is disabled.') end
@@ -9,7 +30,16 @@ function World_LoadWorld()
 
   local _table = File_LoadJSONTable('world.json')
   for _,v in pairs(_table) do
-    CreateObject(v['modelID'], v['x'], v['y'], v['z'], v['rx'], v['ry'], v['rz'], v['sx'], v['sy'], v['sz'])
+    if v['modelID'] ~= nil then
+      CreateObject(v['modelID'], v['x'], v['y'], v['z'], v['rx'], v['ry'], v['rz'], v['sx'], v['sy'], v['sz'])
+    else
+      local _AddYaw = DoorConfig[tonumber(doorID)]
+      if _AddYaw == nil then
+        _AddYaw = 90
+      end
+
+      CreateDoor(v['doorID'], v['x'], v['y'], v['z'], v['yaw'] + _AddYaw, true)
+    end
   end
 
   print('Server: World loaded!')
